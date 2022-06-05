23b4dff02697f126cb394a628aa4ed9d7b979ca2
Reece431 <Farnworthwork@gmail.com>
2022-06-05 17:55:10 +0100

    seperted files

--- src/script.js
+++ src/script.js
@@ -4,9 +4,6 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { Vector3 } from 'three'
 
-import * as tf from "@tensorflow/tfjs";
-import * as handpose from "@tensorflow-models/handpose";
-
 /**
  * RAYCASTING
  * cast a ray in a specific direction and test what objects intersect with it.
@@ -158,8 +155,6 @@ let currentIntersect = null;
 const tick = () =>
 {
 
-    runHandpose();
-
     const elapsedTime = clock.getElapsedTime()
 
     // Animate spheres
@@ -221,55 +216,5 @@ const tick = () =>
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
-
-//---------classic js
-const video = document.querySelector('.video');
-const constraints = {
-    audio: false,
-    video: {
-        width: {min: 1024, ideal: 1200, max: 1920},
-        height: {min: 576, ideal: 720, max: 1080}
-    }
-}
-
-const runHandpose = async () => {
-    const net = await handpose.load();
-    console.log("Handpose model loaded.");
-    //  Loop and detect hands
-    setInterval(() => {
-      detect(net);
-    }, 100);
-  };
-
-  const detect = async (net) => {
-      const video = document.querySelector('.video')
-      console.log(video)
-    // Check data is available
-    if (
-      typeof video !== "undefined" &&
-      video !== null &&
-      video.readyState === 4
-    ) {
-      // Make Detections
-      const hand = await net.estimateHands(video);
-    //   console.log(hand);
-
-      // Draw mesh
-    //   drawHand(hand);
-    }
-  };
-
-async function startWebcam() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        video.srcObject = stream;
-        video.stream = stream;
-        console.log(stream)
-    } catch(err) {
-        console.log(err)
-    }
-}
-
-startWebcam()
 
 tick()
